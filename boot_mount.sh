06c016ff9755eeacc44f5769286ed7000eba82a7
georgi123 <gogo3888@mail.bg>
2018-12-20 13:46:27 +0200

    Create boot_mount.sh

---
+++ boot_mount.sh
@@ -0,0 +1,9 @@
+CONF=etc
+OLDSLES=mnt
+LOG=set_configuration.log
+
+#mount suse11 disk in the /mnt
+mount /dev/$1/root /$OLDSLES/
+mount /dev/$1/opt /$OLDSLES/opt/
+mount /dev/$1/tmp /$OLDSLES/tmp/
+mount /dev/$1/home /$OLDSLES/usr2/local/
