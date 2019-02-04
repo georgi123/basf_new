33277105bd998c8accfe287e101b7c3b79b5a241
georgi123 <gogo3888@mail.bg>
2019-02-04 14:31:40 +0200

    Create unregister.pl

---
+++ unregister.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+#####use the script when have to revert from sles12 to sles11#######
+
+##unregister server from APX#####
+system('/opt/apxpccp/pccprel exec,DCM/DCM_DEREG_$(hostname).scp');
+system('/opt/apxpccc/pccCrel exec,DCM/DCM_DEREG_$(hostname).scp');
+##unregister server from HPSA##
+system('/opt/opsware/agent/bin/agent_uninstall.sh --no_deactivate --force');
+system('rm -r /opt/opsware');
+##unjoin server from BASF AD##
+system('/root/scripts/sles_migration/basfad/./basfad_join2AD.sh -f $(hostname)');
