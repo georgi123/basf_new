cb52f6719ab2f4a079111f34b15dcb2ede1799d6
georgi123 <gogo3888@mail.bg>
2019-02-06 15:56:08 +0200

    Create register.pl

---
+++ register/register.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+#####use the script when have to revert from sles12 to sles11#######
+
+##unregister server from APX#####
+system('/opt/apxpccc/recreate_pccc_pks.sh');
+system('/opt/apxpccp/recreate_pccp_pks.sh');
+##unregister server from HPSA##
+system('./opsware-agent-60.0.71639.1-linux-SLES-12-X86_64 --loglevel info --opsw_gw_addr_list 10.92.96.41:3001 -f --force_new_device');
+
+##unjoin server from BASF AD##
+system('/root/scripts/sles_migration/basfad/./basfad_join2AD.sh -u $(hostname)');
